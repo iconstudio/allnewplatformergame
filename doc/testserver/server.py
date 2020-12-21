cc506030e45124261443414bda5b9f7f414136b6
iconstudio <yoyofa2@hotmail.com>
2020-12-21 17:08:26 +0900

    * The test server is modified.

--- doc/testserver/server.py
+++ doc/testserver/server.py
@@ -1,21 +1,41 @@
 """
 	Project: Server
 """
-from flask import Flask, escape, request
+from flask import Flask, escape, request, jsonify
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
-name = "iconer"
-"".encode
+api = Api(app)
 
 @app.route("/")
 def hello():
-	 name = request.args.get("name", "World")
-	 return f'Hello, {escape(name)}!'
+	name = request.args.get("name", "World")
+	return f'Hello, {escape(name)}!'
 
-@app.route("/sun")
-def subroot1():
-	print("subroot1")
-	return "subroot1"
+@app.route("/subroot1")
+def fn_subroot1():
+	print("subroot1-py-msg")
+	return "subroot1-js-log"
+
+@app.route('/user/login', methods = ['POST'])
+def userLogin():
+	user = request.get_json() # json 데이터를 받아옴
+	return jsonify(user) # 받아온 데이터를 다시 전송
+
+class RegistUser(Resource):
+	"""Making a new restful api for URI 'user'.
+	"""
+
+	def post(self):
+		return {'result': 'ok'}
+
+"""app_route("/user")
+"""
+api.add_resource(RegistUser, '/user')
+ 
+@app.route('/environments/<language>')
+def environments(language):
+	return jsonify({"language":language})
 
 if __name__ == "__main__":
-	pass
+	app.run()
