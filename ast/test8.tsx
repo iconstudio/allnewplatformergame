b0505610f35aa4c701b1353bd3e9c10df8f444c2
iconstudio <yoyofa2@hotmail.com>
2021-02-15 01:05:52 +0900

    + External level test case

---
+++ ast/test8.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="test8" tilewidth="8" tileheight="8" margin="1" tilecount="3969" columns="63" objectalignment="topleft">
+ <image source="../collectgame/sprites/sTilesetTerrain/2dcad0a5-064f-4ad1-8668-e6a48539462c.png" width="512" height="512"/>
+ <terraintypes>
+  <terrain name="Dirt" tile="74"/>
+ </terraintypes>
+</tileset>
